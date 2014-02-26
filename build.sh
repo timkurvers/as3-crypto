f91e60c3c0aff48a681334419b43cb33be4b68e6
mangui <g.du.pontavice@gmail.com>
2014-02-26 15:13:58 +0800

    word based AES encryption/decryption derived from https://code.google.com/p/crypto-js/source/browse/tags/3.1.2/src/aes.js

--- build.sh
+++ build.sh
@@ -1,9 +1,3 @@
 #FLEXPATH=../../flex_sdk_4.6
 FLEXPATH=../../AIRSDK_Compiler
-$FLEXPATH/bin/compc -include-sources src -output deploy/as3crypto.swc -use-network=false -optimize=true -incremental=false -target-player="10.1" -static-link-runtime-shared-libraries=true -debug=false
-cp deploy/as3crypto.swc deploy/as3crypto.zip
-unzip -o deploy/as3crypto.zip
-./azoth.exe library.swf library.swf
-zip tmp.zip library.swf catalog.xml
-rm -rf library.swf catalog.xml deploy/as3crypto.swc deploy/as3crypto.zip
-mv tmp.zip deploy/as3crypto.swc
+$FLEXPATH/bin/compc -include-sources src -output deploy/as3crypto.swc -use-network=false -optimize=true -target-player="10.1" -static-link-runtime-shared-libraries=true
